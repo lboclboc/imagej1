dbd35660bfe1261a8f296b0f7294e26e0eda400b
Lars Berntzon <lars.berntzon@cecilia-data.se>
2020-03-05 23:33:46 +0100

    Imported AIJ-3.2.0

--- ij/plugin/FITS_Reader.java
+++ ij/plugin/FITS_Reader.java
@@ -6,6 +6,7 @@ import ij.*;
 import ij.io.*;
 import ij.process.*;
 import ij.measure.*;
+import ij.util.Tools;
 
 /** Opens and displays FITS images. The FITS format is 
 	described at "http://fits.gsfc.nasa.gov/fits_standard.html".
@@ -23,6 +24,7 @@ public class FITS_Reader extends ImagePlus implements PlugIn {
 		FileInfo fi = null;
 		try {fi = fd.getInfo();}
 		catch (IOException e) {}
+        
 		if (fi!=null && fi.width>0 && fi.height>0 && fi.offset>0) {
 			FileOpener fo = new FileOpener(fi);
 			ImagePlus imp = fo.open(false);
@@ -37,12 +39,32 @@ public class FITS_Reader extends ImagePlus implements PlugIn {
 			  setStack(fileName, stack);
 			}
 			Calibration cal = imp.getCalibration();
-			if (fi.fileType==FileInfo.GRAY16_SIGNED && fd.bscale==1.0 && fd.bzero==32768.0)
+            
+            if (fi.fileType==FileInfo.GRAY8 && (fd.bscale!=1.0 || fd.bzero!=0))
+                {  
+                cal.setFunction(Calibration.STRAIGHT_LINE, new double[] {fd.bzero, fd.bscale}, "Gray Value");
+                }
+            else if (fi.fileType==FileInfo.GRAY16_SIGNED && fd.bscale==1.0 && fd.bzero==32768.0)
+                {
 				cal.setFunction(Calibration.NONE, null, "Gray Value");
+                }
+            else if (fi.fileType==FileInfo.GRAY16_SIGNED && fd.bscale!=0.0) // && (fd.bscale!=1.0 || fd.bzero!=0.0))
+                {
+                cal.setFunction(Calibration.STRAIGHT_LINE, new double[] {fd.bzero-32768.0*fd.bscale, fd.bscale}, "Gray Value");
+                }
+            else if ((fi.fileType==FileInfo.GRAY32_FLOAT || fi.fileType==FileInfo.GRAY32_INT || 
+                      fi.fileType==FileInfo.GRAY64_FLOAT) && (fd.bscale!=1.0 || fd.bzero!=0.0)) 
+                {    //all of these data types are converted to 32-float by ImageReader before reaching this point
+                ImageProcessor ip = imp.getProcessor();
+                float[] pixels = (float[])ip.getPixels();
+                for (int i = 0; i < pixels.length; i++)
+                    pixels[i] = (float)(fd.bzero + fd.bscale*pixels[i]);
+                imp.setProcessor(ip);   
+                }            
 			setCalibration(cal);
 			setProperty("Info", fd.getHeaderInfo());
 			setFileInfo(fi); // needed for File->Revert
-			if (arg.equals("")) show();
+            if (arg.equals("")) show();
 		} else
 			IJ.error("This does not appear to be a FITS file.");
 		IJ.showStatus("");
@@ -54,7 +76,7 @@ class FitsDecoder {
 	private String directory, fileName;
 	private DataInputStream f;
 	private StringBuffer info = new StringBuffer(512);
-	double bscale, bzero;
+	double bscale = 1.0, bzero = 0.0;
 
 	public FitsDecoder(String directory, String fileName) {
 		this.directory = directory;
@@ -130,22 +152,22 @@ class FitsDecoder {
 			else if (key.equals("NAXIS3")) //for multi-frame fits
 				fi.nImages = Integer.parseInt ( value );
 			else if (key.equals("BSCALE"))
-				bscale = parseDouble ( value );
+				bscale = Tools.parseDouble(value, 1.0);
 			else if (key.equals("BZERO"))
-				bzero = parseDouble ( value );
-		else if (key.equals("CDELT1"))
-				fi.pixelWidth = parseDouble ( value );
-		else if (key.equals("CDELT2"))
-				fi.pixelHeight = parseDouble ( value );
-		else if (key.equals("CDELT3"))
-				fi.pixelDepth = parseDouble ( value );
-		else if (key.equals("CTYPE1"))
-				fi.unit = value;
+				bzero = Tools.parseDouble(value, 0.0);
+//            else if (key.equals("CDELT1"))                //LET WCS TAKE CARE OF ADDING THIS INFO
+//                fi.pixelWidth = Math.abs(Tools.parseDouble(value, 1.0));
+//            else if (key.equals("CDELT2"))
+//                fi.pixelHeight = Math.abs(Tools.parseDouble(value, 1.0));
+//            else if (key.equals("CDELT3"))
+//                    fi.pixelDepth = parseDouble ( value );
+//            else if (key.equals("CTYPE1"))
+//                fi.unit = value;
 
 			if (count>360 && fi.width==0)
 				{f.close(); return null;}
 		}
-
+        if (fi.fileType==FileInfo.GRAY32_INT && bzero == 2147483648.0 && bscale == 1.0) fi.fileType = FileInfo.GRAY32_UNSIGNED;
 		f.close();
 		fi.offset = 2880+2880*(((count*80)-1)/2880);
 		return fi;
